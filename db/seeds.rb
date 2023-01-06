d6d639ff8ca4d716251ab82b6a0ab294bf0b99ce
samiullah997 <samiullahk997@gmail.com>
2023-01-06 17:15:26 +0500

    Generate migration for greeting and puts data in seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(name: "Good morning.")
+Greeting.create(name: "Goodbye.")
+Greeting.create(name: "See you tomorrow.")
+Greeting.create(name: "Good evening.")
+Greeting.create(name: "Good afternoon.")
+Greeting.create(name: "See you soon.")
+
