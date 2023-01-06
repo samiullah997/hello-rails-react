19a22acddd3451755b3c7d2e41fa93aab69da923
Sami Ullah <samiullahk997@gmail.com>
2023-01-06 20:53:59 +0500

    Update App.js
    Update app.js file and user react router for routes.

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
-import Greeting from './Greeting'
-import store from './redux/config/store'
-import { Provider } from 'react-redux'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Greeting from './Greeting';
+import store from './redux/config/store';
 
-const App = () => {
-  return (
-      <Provider store={store}>
-          <Greeting />
-      </Provider>
-  )
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </div>
+    </Router>
+  </Provider>
+);
 
 export default App;
